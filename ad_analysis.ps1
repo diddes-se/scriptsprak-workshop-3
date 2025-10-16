0d5761c3c0695aad0254675b270b2082ba95fc56
diddes-se <github@diddes.se>
2025-10-16 14:48:57 +0200

    added Get-SafeDate, Executive summary in report, added user account that soon expires, added  computers not seen for 30 days, added users with old passwords

--- ad_analysis.ps1
+++ ad_analysis.ps1
@@ -1,16 +1,28 @@
 # Import the JSON file
 $data = Get-Content -Path "ad_export.json" -Raw | ConvertFrom-Json
 
+function Get-SafeDate {
+    param($DateString)
+    try {
+        return [datetime]$DateString
+    }
+    catch {
+        Write-Warning "Ogiltigt datum: $DateString"
+        return $null
+    }
+}
+
+
 # Make a variable for todays date
 $now = Get-Date
 
-# Create variables to store data
+# Create a hash table to store user data
 $ad_export_users = @()
 
 # Loop thru users and add them to $ad_export_users
 foreach ($user in $data.users) {
-    $lastLogon = [datetime]$user.lastLogon
-    $lastPassSet = [datetime]$user.passwordLastSet
+    $lastLogon = Get-SafeDate($user.lastLogon)
+    $lastPassSet = Get-SafeDate($user.passwordLastSet)
     $daysSinceLastLogon = ($now - $lastLogon).Days
     $daysSinceLastPass = ($now - $lastPassSet).Days
 
@@ -35,20 +47,22 @@ foreach ($user in $data.users) {
 # Sort $ad_export after days sence password set
 $sorted_ad_export_users_lastPass = $ad_export_users | Sort-Object DaysSincePasswordSet -Descending
 
-
 # Group users per department
 $users_per_department = $data.users | Group-Object -Property department
 
-# Get inactive users hwo has not logged in for 30 days
+# Get inactive users hwo has not logged in for 30+ days
 $inactiveUsers = $ad_export_users | Where-Object { $_.lastLogon -lt $now.AddDays(-30) }
 
 # Sort computers after last login in descending order and filter out the 10 with longest time
-$sortedComputers = $data.computers | Sort-Object -Property lastLogon -Descending | Select-Object -first 10
+$sortedComputersLastLogon = $data.computers | Sort-Object -Property lastLogon -Descending | Select-Object -first 10
 
-Write-Host $sortedComputers
-# Get the 10 computers who has longest time since checkin
+# Get user account that expires within 30 days
+$accountExpieringsSoon = $ad_export_users | Where-Object { $_.AccountExpires -lt $now.AddDays(30) }
 
+# Get the computers who hasn't log on for 90+ days
+$computersNotSeenForLong = $data.computers | Where-Object { $_.lastLogon -lt $now.AddDays(-30) }
 
+$oldPass = $ad_export_users | Where-Object { $_.PasswordLastSet -lt $now.AddDays(-90) }
 
 # Export to inactive_users.csv
 $inactiveUsers | Select-Object samAccountName, displayName, Department, Site, LastLogon, DaysInactive, AccountExpires |
@@ -60,13 +74,18 @@ Export-Csv -Path "days_since_password_set.csv" -NoTypeInformation -Encoding UTF8
 
 # Create formated report
 $report = @"
-
 ACTIVE DIRECTORY AUDIT
-
-======================
-Rapporten genererad: $($now)
+$("=" * 40)
+Genererad: $($now)
 Domän: $($data.domain)
 Exporterad: $($data.export_date)
+
+Executive summary
+$("-" * 40)
+Critical: $($accountExpieringsSoon.count) konton förfaller inom 30 dagar
+Varning: $($inactiveUsers.Count) användare har inte loggat in på 30+ dagar
+Varning: $($computersNotSeenForLong.count) datorer har inte loggat in på 30+ dagar
+Säkerhet: $($oldPass.count) användare har lösenord äldre än 90 dagar
 
 Inaktiva användare:
 $("-" * 40)
@@ -80,11 +99,10 @@ foreach ($user in $inactiveUsers) {
     $report += "  " + "{0,-20}  {1,-14} {2,10}  `n" -f $user.displayName, $user.samAccountName, $user.LastLogon #`n"
 }
 
-
-
 $report += "`nAntal användare per avdelning `n"
 $report += $("-" * 40) + "`n"
 
+# add usercount per department
 foreach ($group in $users_per_department) {
     $report += "  {0,-10} {1,3} användare `n" -f $group.Name, $group.Count
 }
@@ -93,7 +111,8 @@ $report += "`nDatorer som inte checkat in på längst tid `n"
 $report += $("-" * 40) + "`n"
 $report += "{0,-17} {1,-14} {2,-20} `n" -f "  Datornamn", "Site", "Senaste inloggning"
 
-foreach ($computer in $sortedComputers) {
+# Add computers who hasn't log on for a long time
+foreach ($computer in $sortedComputersLastLogon) {
     $report += "  " + "{0,-15} {1,-14} {2,-20} `n" -f $computer.Name, $computer.site, $computer.lastLogon
 }
 
